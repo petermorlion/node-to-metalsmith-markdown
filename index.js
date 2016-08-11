36bd94a84a495b770d45fb2c61480e4f8564f5e4
Peter Morlion <peter.morlion@gmail.com>
2016-08-11 09:33:13 +0200

    Add script and package.json

---
+++ index.js
@@ -0,0 +1,40 @@
+var sqlite = require('sqlite-sync');
+var fs = require('fs');
+var rimraf = require('rimraf');
+
+var destination = './build';
+
+var clearDestination = function(callback) {
+  rimraf(destination, {}, callback);
+};
+
+var generateFiles = function() {
+
+  if (!fs.existsSync(destination)){
+      fs.mkdirSync(destination);
+  }
+
+  sqlite.connect('./ghost.db');
+
+  var posts = sqlite.run('SELECT p.id, p.title, p.markdown, p.slug, p.status FROM \'posts\' p');
+
+  posts.forEach(function(post){
+    var fileName = `${destination}/${post.slug}.md`;
+    var tags = sqlite.run(`SELECT t.name FROM 'tags' t INNER JOIN 'posts_tags' pt on pt.tag_id = t.id WHERE pt.post_id = ${post.id}`);
+
+    var tagsDelimited = tags.map(function(tag) { return tag.name }).join(',');
+
+    var data = `---
+title: ${post.title}
+tags: ${tagsDelimited}
+draft: ${post.status === 'draft'}
+---
+${post.markdown}`;
+
+    fs.writeFile(fileName, data, {}, function() {
+      console.log('Created ' + fileName);
+    });
+  });
+};
+
+clearDestination(generateFiles);
