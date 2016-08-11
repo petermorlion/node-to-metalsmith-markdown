bccc58fceeab2116bbd356ebfad595279952cac1
Peter Morlion <peter.morlion@gmail.com>
2016-08-11 11:18:38 +0200

    Add date to post

--- index.js
+++ index.js
@@ -16,18 +16,23 @@ var generateFiles = function() {
 
   sqlite.connect('./ghost.db');
 
-  var posts = sqlite.run('SELECT p.id, p.title, p.markdown, p.slug, p.status FROM \'posts\' p');
+  var posts = sqlite.run('SELECT p.id, p.title, p.markdown, p.slug, p.status, p.published_at FROM \'posts\' p');
 
   posts.forEach(function(post){
     var fileName = `${destination}/${post.slug}.md`;
     var tags = sqlite.run(`SELECT t.name FROM 'tags' t INNER JOIN 'posts_tags' pt on pt.tag_id = t.id WHERE pt.post_id = ${post.id}`);
 
     var tagsDelimited = tags.map(function(tag) { return tag.name }).join(',');
+    var publishDate = new Date(post.published_at);
+    var year = publishDate.getFullYear();
+    var month = `0${publishDate.getMonth()}`.slice(-2);
+    var day = `0${publishDate.getDay()}`.slice(-2);
 
     var data = `---
 title: ${post.title}
 tags: ${tagsDelimited}
 draft: ${post.status === 'draft'}
+date: ${year}-${month}-${day}
 ---
 ${post.markdown}`;
 
